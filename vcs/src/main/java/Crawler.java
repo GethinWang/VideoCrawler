d20d0056282bb5dd34369537d2dc61817567909c
wgx <735347452@qq.com>
2019-09-03 00:00:42 +0800

    190903

--- vcs/src/main/java/Crawler.java
+++ vcs/src/main/java/Crawler.java
@@ -1,33 +1,34 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import java.io.*;
 
 public class Crawler {
 
-    public static void main(String[] args) throws Exception{
-        String url = "http://www.le.com/ptv/vplay/31628417.html";
+    public static String rootPath = "";
+
+    static {
+        rootPath = Crawler.class.getResource("").getPath().substring(1).replace("target/classes/", "src/main/resources");
+    }
+
+    public static void main(String[] args) throws Exception {
+        String url = "https://www.acfun.cn/bangumi/ab5025182";
         Runtime rt = Runtime.getRuntime();
-        String exec = "C:\\Users\\10007886\\Desktop\\phantomjs-2.1.1-windows\\bin\\phantomjs C:\\Users\\10007886\\Desktop\\phantomjs-2.1.1-windows\\bin\\code.js " + url;
+        String exec = rootPath + "/phantomjs.exe " + rootPath + "/code.js " + url;
         Process p = rt.exec(exec);
         InputStream is = p.getInputStream();
         BufferedReader br = new BufferedReader(new InputStreamReader(is));
         StringBuffer sbf = new StringBuffer();
         String tmp = "";
-        while((tmp = br.readLine())!=null){
+        while ((tmp = br.readLine()) != null) {
             sbf.append(tmp);
         }
-        //System.out.println(sbf.toString());
-//        return sbf.toString();
-        writeFile("C:\\Users\\10007886\\Desktop\\demo.txt",sbf.toString());
-
-//        Document doc = Jsoup.parse(is, "UTF-8", url);
-//        Elements elements = doc.getAllElements();
-//        StringBuffer stringBuffer = new StringBuffer();
-//        for (Element element : elements) {
-//            stringBuffer.append(element.attributes()).append("\n");
-////            System.out.println(element.attributes());
-//        }
-//        writeFile("C:\\Users\\10007886\\Desktop\\demo.txt",stringBuffer.toString());
+        writeFile("C:\\Users\\wgx\\Desktop\\demo.txt", sbf.toString());
     }
-    public static void writeFile(String pathname,String content) {
+
+    public static void writeFile(String pathname, String content) {
         try {
             File writeName = new File(pathname);
             FileWriter writer = new FileWriter(writeName);
