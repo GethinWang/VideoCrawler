710ea0976bef497f0e8c47d3351b62ce0d925305
wanggx <wanggx@startimes.com.cn>
2019-09-02 17:04:20 +0800

    19080902

--- vcs/src/main/java/Crawler.java
+++ vcs/src/main/java/Crawler.java
@@ -1,26 +1,31 @@
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
 import java.io.*;
 
 public class Crawler {
 
     public static void main(String[] args) throws Exception{
-        String url = "https://www.csdn.net";
+        String url = "http://www.le.com/ptv/vplay/31628417.html";
         Runtime rt = Runtime.getRuntime();
         String exec = "C:\\Users\\10007886\\Desktop\\phantomjs-2.1.1-windows\\bin\\phantomjs C:\\Users\\10007886\\Desktop\\phantomjs-2.1.1-windows\\bin\\code.js " + url;
         Process p = rt.exec(exec);
         InputStream is = p.getInputStream();
-        Document doc = Jsoup.parse(is, "UTF-8", url);
-        Elements elements = doc.getAllElements();
-        StringBuffer stringBuffer = new StringBuffer();
-        for (Element element : elements) {
-            stringBuffer.append(element.attributes()).append("\n");
-//            System.out.println(element.attributes());
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        StringBuffer sbf = new StringBuffer();
+        String tmp = "";
+        while((tmp = br.readLine())!=null){
+            sbf.append(tmp);
         }
-        writeFile("C:\\Users\\10007886\\Desktop\\demo.txt",stringBuffer.toString());
+        //System.out.println(sbf.toString());
+//        return sbf.toString();
+        writeFile("C:\\Users\\10007886\\Desktop\\demo.txt",sbf.toString());
+
+//        Document doc = Jsoup.parse(is, "UTF-8", url);
+//        Elements elements = doc.getAllElements();
+//        StringBuffer stringBuffer = new StringBuffer();
+//        for (Element element : elements) {
+//            stringBuffer.append(element.attributes()).append("\n");
+////            System.out.println(element.attributes());
+//        }
+//        writeFile("C:\\Users\\10007886\\Desktop\\demo.txt",stringBuffer.toString());
     }
     public static void writeFile(String pathname,String content) {
         try {
