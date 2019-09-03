4807f53cefc159ba452e3ec4a78d3b4ae0841edb
wanggx <wanggx@startimes.com.cn>
2019-09-03 15:49:19 +0800

    19080903 爬取视频地址并下载

--- vcs/src/main/java/Main.java
+++ vcs/src/main/java/Main.java
@@ -15,11 +15,11 @@ public class Main {
     //下载视频
     public static void main(String[] args) {
         try {
-//            String url = "https://www.acfun.cn/bangumi/ab5025182";
-//            List<String> urls = new Crawler().getVideoUrls(url);
+            String url = "https://www.acfun.cn/bangumi/ab5025182";
+            List<String> urls = new Crawler().getVideoUrls(url);
 //            //选择输出路径，文件夹
             String output = "C:/Users/10007886/Desktop/a1.mp4";
-            Downloader.downloadFile("https://tx-video.acfun.cn/mediacloud/acfun/acfun_video/segment/R0Jb5JHC1jheBaDjBYV4iNX0-uxVwiuat084zhbVYfjKRE1-O_KT0ETzIbc39JH8.m3u8?sign=6522010b8352106960f81757f1c9f08d&t=1567513664&us=684510119",output);
+            Downloader.downloadFile(urls.get(0),output);
         }catch (Exception e){
             System.out.println("ERROR!"+e);
         }
